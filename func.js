70fa1c5a3dd3ed099620a4e62ffab624b16fbb6c
Anubic29 <asitailo2039@gmail.com>
2022-01-02 18:04:05 +0200

    feat: add pop-up menu

--- func.js
+++ func.js
@@ -6,6 +6,7 @@ window.onload=function() {
     } else {
       btnToTop.classList.remove('show');
     }
+    popUpMenu.classList.remove('active');
   });
   function scrollToTop() {
     document.documentElement.scrollTo({
@@ -14,6 +15,19 @@ window.onload=function() {
     });
   }
   btnToTop.addEventListener("click", scrollToTop);
+
+  var btnMenu = document.getElementById("btnMenu");
+  var popUpMenu = document.getElementById("popUpMenu");
+  var links = document.getElementsByTagName("a")
+  btnMenu.addEventListener("click", function() {
+    if(!popUpMenu.classList.contains('active')) {
+      popUpMenu.classList.add('active');
+    } else {
+      popUpMenu.classList.remove('active');
+    }
+  });
+  links.addEventListener("click", popUpMenu.classList.remove('active'));
+  
 }
 
 
